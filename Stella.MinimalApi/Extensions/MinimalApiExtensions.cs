57ea28f1c5a241e9fe2795f5312d74170ea14221
KerimCetinbas <a.cetinbas@icloud.com>
2025-08-08 01:05:56 +0300

    initial

---
+++ Stella.MinimalApi/Extensions/MinimalApiExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+namespace Stella.MinimalApi.Extensions;
+
+public static class MinimalApiExtensions
+{
+    public static IServiceCollection AddEndpoints(this IServiceCollection services, Assembly assembly)
+    {
+        var endpointServiceDesciptors =  assembly
+            .DefinedTypes
+            .Where(type => type is { IsAbstract: false, IsInterface: false } &&
+                           typeof(IEndpoint).IsAssignableFrom(type))
+            .Select(type => ServiceDescriptor.Transient(typeof(IEndpoint), type)).ToArray();
+
+        
+        services.TryAddEnumerable(endpointServiceDesciptors);
+        return services;
+    }
+
+    public static IApplicationBuilder MapEndpoints(
+        this WebApplication app,
+        RouteGroupBuilder? routeGroupBuilder = null)
+    {
+        var endpoints = app.Services.GetRequiredService<IEnumerable<IEndpoint>>();
+
+        IEndpointRouteBuilder builder = routeGroupBuilder is null ? app : routeGroupBuilder;
+        foreach (var endpoint in endpoints)
+        {
+            endpoint.MapEndpoint(builder);
+        }
+        return app;
+    }
+}
---
+++ Stella.MinimalApi/IEndpoint.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Routing;
+
+namespace Stella.MinimalApi;
+
+public interface IEndpoint
+{
+    void MapEndpoint(IEndpointRouteBuilder app);
+}
